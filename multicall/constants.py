4ffba7ff20d70d7991048c5701f00101b8948866
banteg <banteeg@gmail.com>
2020-01-28 15:14:14 +0700

    add multicall addresses

---
+++ multicall/constants.py
@@ -0,0 +1,18 @@
+from enum import IntEnum
+
+
+class Network(IntEnum):
+    Mainnet = 1
+    Kovan = 42
+    Rinkeby = 4
+    Görli = 5
+    xDai = 100
+
+
+MULTICALL_ADDRESS = {
+    Network.Mainnet: '0xeefBa1e63905eF1D7ACbA5a8513c70307C1cE441',
+    Network.Kovan: '0x2cc8688C5f75E365aaEEb4ea8D6a480405A48D2A',
+    Network.Rinkeby: '0x42Ad527de7d4e9d9d011aC45B31D8551f8Fe9821',
+    Network.Görli: '0x77dCa2C955b15e9dE4dbBCf1246B4B85b651e50e',
+    Network.xDai: '0xb5b692a88BDFc81ca69dcB1d924f59f0413A602a',
+}
