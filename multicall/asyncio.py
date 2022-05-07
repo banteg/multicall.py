517a53cf3897f2854caba20df76658c724de51e8
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2022-05-07 02:57:59 +0000

    fix: run loop in foreground

--- multicall/asyncio.py
+++ multicall/asyncio.py
@@ -7,10 +7,8 @@ from web3 import AsyncHTTPProvider, Web3
 from web3.eth import AsyncEth
 
 async_w3s: Dict[Web3,Web3] = {}
-
-def start_background_loop(loop: asyncio.AbstractEventLoop) -> None:
-    asyncio.set_event_loop(loop)
-    loop.run_forever()
+async_loop = asyncio.get_event_loop()
+process_pool_executor = ProcessPoolExecutor(16)
 
 def get_async_w3(w3: Web3) -> Web3:
     if w3 in async_w3s:
@@ -20,7 +18,4 @@ def get_async_w3(w3: Web3) -> Web3:
     async_w3s[w3] = async_w3
     return async_w3
 
-async_loop = asyncio.new_event_loop()
-threading.Thread(target=start_background_loop, args=(async_loop,), daemon=True).start()
 
-process_pool_executor = ProcessPoolExecutor(16)
