2aec98348c424ecce5a355e7dd2c8b5a3f370c81
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2022-05-07 03:20:29 +0000

    chore: replace eth_retry.auto_retry_async with eth_retry.auto_retry as it now supports sync and async functions

--- multicall/call.py
+++ multicall/call.py
@@ -95,7 +95,7 @@ class Call:
             self.returns,
         )
 
-    @eth_retry.auto_retry_async
+    @eth_retry.auto_retry
     async def async_call(self, args: Optional[Any] = None, _w3: Optional[Web3] = None) -> Any:
         _w3 = self.w3 or _w3 or w3
 
