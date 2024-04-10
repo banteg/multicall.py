70eb827a9bc137489d7f25bd979f42502fe4c9ee
banteg <4562643+banteg@users.noreply.github.com>
2024-04-11 00:14:08 +0400

    upgrade (#97)
    * chore: upgrade
    
    * test: fix import
    
    * chore: update comment
    
    ---------
    
    Co-authored-by: BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>

--- tests/test_signature.py
+++ tests/test_signature.py
@@ -1,23 +1,23 @@
-from eth_abi import encode_abi
 from multicall import Signature
+from multicall.signature import encode
 
-args = ((1, 2, 3), '0x' + 'f' * 40, b'data')
-types = ['uint256[]', 'address', 'bytes']
+args = ((1, 2, 3), "0x" + "f" * 40, b"data")
+types = ["uint256[]", "address", "bytes"]
 
 
 def test_signature_parsing():
-    sig = Signature('aggregate((address,bytes)[])(uint256,bytes[])')
-    assert sig.function == 'aggregate((address,bytes)[])'
-    assert sig.input_types == ['(address,bytes)[]']
-    assert sig.output_types == ['uint256','bytes[]']
+    sig = Signature("aggregate((address,bytes)[])(uint256,bytes[])")
+    assert sig.function == "aggregate((address,bytes)[])"
+    assert sig.input_types == ["(address,bytes)[]"]
+    assert sig.output_types == ["uint256", "bytes[]"]
 
 
 def test_signature_encoding():
-    sig = Signature('test(uint256[],address,bytes)()')
-    assert sig.encode_data(args) == sig.fourbyte + encode_abi(types, args)
+    sig = Signature("test(uint256[],address,bytes)()")
+    assert sig.encode_data(args) == sig.fourbyte + encode(types, args)
 
 
 def test_signature_decoding():
-    sig = Signature('test()(uint256[],address,bytes)')
-    data = encode_abi(types, args)
+    sig = Signature("test()(uint256[],address,bytes)")
+    data = encode(types, args)
     assert sig.decode_data(data) == args
