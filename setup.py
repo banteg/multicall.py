dee800b1eac23a72da09a39234b51d4c37824b60
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-05-28 18:08:51 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -172,7 +172,6 @@ setup(
     name=poetry_config["name"].replace("-", "_"),
     version=poetry_config["version"],
     description=poetry_config["description"],
-    python_requires=">=3.8,<4",
     packages=find_packages(),
     package_data={"multicall": ["py.typed"]},
     include_package_data=True,
