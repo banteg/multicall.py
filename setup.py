126f15b6215012b7443a0dd2c8311134020b64ae
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2025-09-04 23:41:01 +0000

    chore: bump version to 0.15.0

--- setup.py
+++ setup.py
@@ -173,7 +173,7 @@ with open("readme.md", "r", encoding="utf-8") as readme:
 
 setup(
     name=poetry_config["name"].replace("-", "_"),
-    version="0.14.1",
+    version="0.15.0",
     description=poetry_config["description"],
     python_requires=">=3.10,<4",
     packages=find_packages(),
