bf2aa51ac46933956856594c94e2de0dd67a21f5
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2026-05-29 18:28:31 -0400

    chore: bump version to 0.16.0

--- multicall/__init__.py
+++ multicall/__init__.py
@@ -4,6 +4,6 @@ from multicall.call import Call
 from multicall.multicall import Multicall
 from multicall.signature import Signature
 
-__version__: Final = "0.15.2"
+__version__: Final = "0.16.0"
 
 __all__ = ["Call", "Multicall", "Signature", "__version__"]
